351334fa9d52f86fad3d8e043cb2e4a5b6641958
Ponmani Thiruvasagam Pandian <ptp@ebner.cc>
2025-04-17 17:53:36 +0530

    feat: add environment configuration files for development

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  //here the actual swagger url will be http://localhost:5254/api/v1. but this api/v1 will automatically be added by the swagger
+  // so we need to remove it from the url
+  apiBaseUrl: 'http://localhost:5254'
+};
